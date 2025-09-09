9a49730891a4abeb209649ae4de34650ce53f18a
Yesica Giraldo <yagiraldot@ieee.org>
2025-09-09 17:53:03 -0500

    creacion de aplicaciones y modelos

--- bookings/models.py
+++ bookings/models.py
@@ -1,3 +1,25 @@
 from django.db import models
+from doctors.models import Doctor
+from patients.models import Patient
 
-# Create your models here.
+class Appointment(models.Model):
+    patient = models.ForeignKey(
+        Patient, related_name='appointments', on_delete=models.CASCADE
+        )
+    doctor = models.ForeignKey(
+        Doctor, related_name='appointments', on_delete=models.CASCADE
+        )
+    appointment_date = models.DateTimeField()
+    appointment_time = models.TextField()
+    notes = models.TextField()
+    status = models.CharField(max_length=10)
+
+class MedicalNote(models.Model):
+    appointment = models.ForeignKey(
+        Appointment, related_name='medical_notes', on_delete=models.CASCADE
+        )
+    doctor = models.ForeignKey(
+        Doctor, related_name='medical_notes', on_delete=models.CASCADE
+        )
+    note = models.TextField()
+    date = models.DateTimeField()
--- patients/models.py
+++ patients/models.py
@@ -1,3 +1,29 @@
 from django.db import models
 
 # Create your models here.
+class Patient(models.Model):
+    first_name = models.CharField(max_length=30)
+    last_name = models.CharField(max_length=30)
+    date_of_birth = models.DateField()
+    contact_number = models.CharField(max_length=15)
+    email = models.EmailField(unique=True)
+    address = models.TextField()
+    phone_number = models.CharField(max_length=15, blank=True, null=True)
+
+class Insurance(models.Model):
+    patient = models.ForeignKey(
+        Patient,related_name='insurances', on_delete=models.CASCADE
+        )
+    provider = models.CharField(max_length=100)
+    policy_number = models.CharField(max_length=100)
+    expiration_date = models.DateField()
+
+class MedicalRecord(models.Model):
+    patient = models.ForeignKey(
+        Patient, related_name='medical_records', on_delete=models.CASCAD
+        )
+    date = models.DateField()
+    diagnosis = models.TextField()
+    treatment = models.TextField()
+    follow_up_date = models.DateField()
+    
